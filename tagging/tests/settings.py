ceecf79c5220cbaa1e61553fab2b5ac5e2ed656b
Flavio Curella <flavio.curella@gmail.com>
2014-06-05 12:14:15 -0500

    fixed test settings

--- tagging/tests/settings.py
+++ tagging/tests/settings.py
@@ -5,19 +5,24 @@ DEFAULT_CHARSET = 'utf-8'
 
 test_engine = os.environ.get("TAGGING_TEST_ENGINE", "sqlite3")
 
-DATABASE_ENGINE = test_engine
-DATABASE_NAME = os.environ.get("TAGGING_DATABASE_NAME", "tagging_test")
-DATABASE_USER = os.environ.get("TAGGING_DATABASE_USER", "")
-DATABASE_PASSWORD = os.environ.get("TAGGING_DATABASE_PASSWORD", "")
-DATABASE_HOST = os.environ.get("TAGGING_DATABASE_HOST", "localhost")
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.%s' % test_engine,
+        'NAME': os.environ.get("TAGGING_DATABASE_NAME", "tagging_test"),
+        'USER': os.environ.get("TAGGING_DATABASE_USER", ""),
+        'PASSWORD': os.environ.get("TAGGING_DATABASE_PASSWORD", ""),
+        'HOST': os.environ.get("TAGGING_DATABASE_HOST", "localhost")
+    }
+}
 
 if test_engine == "sqlite":
-    DATABASE_NAME = os.path.join(DIRNAME, 'tagging_test.db')
-    DATABASE_HOST = ""
+    DATABASES['default']['NAME'] = os.path.join(DIRNAME, ':memory:')
+    DATABASES['default']['HOST'] = ""
 elif test_engine == "mysql":
-    DATABASE_PORT = os.environ.get("TAGGING_DATABASE_PORT", 3306)
+    DATABASES['default']['PORT'] = os.environ.get("TAGGING_DATABASE_PORT", 3306)
 elif test_engine == "postgresql_psycopg2":
-    DATABASE_PORT = os.environ.get("TAGGING_DATABASE_PORT", 5432)
+    DATABASES['default']['PORT'] = os.environ.get("TAGGING_DATABASE_PORT", 5432)
+
 
 
 INSTALLED_APPS = (
@@ -25,3 +30,5 @@ INSTALLED_APPS = (
     'tagging',
     'tagging.tests',
 )
+
+SECRET_KEY = '1234'
